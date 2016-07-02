fb5e02c9aa863c1bc6cee6a2bc745e8366b89aa7
Renato "Lond" Cerqueira <renato@lond.com.br>
2016-07-02 16:29:07 +0200

    Add update from live site for sinfest channel.

--- app/controllers/sinfest_controller.rb
+++ app/controllers/sinfest_controller.rb
@@ -1,6 +1,53 @@
+require 'curb'
+
 class SinfestController < ApplicationController
+  EXPIRE_AFTER = 24.hours
+  ITEMS_TO_FETCH = 2 # normally will be 7, this is just for initial charge.
+  FIRST_COMIC_DATE = '2000-01-17'.to_date
   def index
     @channel = Channel.find_by_channel_type(Channel.channel_types[:sinfest])
+    if (@channel.last_build_date < Time.now - EXPIRE_AFTER) then
+      update_sinfest
+    end
     render formats: :xml
+  end
+
+  def update_sinfest
+    start_date = ITEMS_TO_FETCH.days.ago
+    if(start_date < FIRST_COMIC_DATE) then
+      start_date = FIRST_COMIC_DATE
+    end
+
+    date = start_date
+    today = Time.now.to_date
+    ActiveRecord::Base.transaction do
+      @channel.channel_items.destroy_all
+
+      while date.to_date < today do
+        pubdate = date.to_date.to_s
+
+        link = "http://www.sinfest.net/view.php?date=#{pubdate}"
+
+        c = Curl::Easy.new(link) do |curl|
+          curl.headers['User-Agent'] = 'rss-generator-1.0'
+        end
+        c.perform
+
+        if(c.status == '200 OK') then
+          html_doc = Nokogiri::HTML(c.body)
+          title = html_doc.xpath('/html/body/div/table[4]/tr[1]/td/table/tbody/tr[1]/td').text
+          description = html_doc.xpath('/html/body/div/table[4]/tr[1]/td/table/tbody/tr[2]/td').children.to_s
+          description.gsub!(/btphp/, 'http://www.sinfest.net/btphp')
+          description.gsub!(/alt/, 'border="0" alt')
+          guid = "sinfest-imgs-#{pubdate}"
+          ChannelItem.create(link: link, pubdate: pubdate, title: title, description: description, channel: @channel, guid: guid)
+        end
+
+        date += 1.day
+      end
+
+      @channel.last_build_date = Time.now
+      @channel.save
+    end
   end
 end
