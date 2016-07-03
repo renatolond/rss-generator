728b2767c3bc2f590cc5da3de38e8c6171f08f5d
Renato "Lond" Cerqueira <renato@lond.com.br>
2016-07-03 13:21:55 +0200

    Change default rss image for sinfest.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,5 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 c = Channel.create(title: 'Sinfest', link: 'http://www.sinfest.net', description: 'Strip dealing with contemporary issues and religion. Created by Tatsuya Ishida.', channel_type: Channel.channel_types[:sinfest], last_build_date: Time.now)
-ChannelImage.create(title: 'Sinfest', url: 'http://www.sinfest.net/favicon.ico', link: 'http://www.sinfest.net', width: 16, height: 16, channel: c)
+ChannelImage.create(title: "Sinfest", url: "http://www.sinfest.net/rssicon.png", link: "http://www.sinfest.net", width: 28, height: 30, channel_id: c.id)
 ChannelItem.create(title: 'July 1, 2016: Sweep 5', link: 'http://www.sinfest.net/view.php?date=2016-07-01', guid: "sinfest.#{SecureRandom.uuid}", pubdate: '2016-07-01', description: '<img src="http://www.sinfest.net/btphp/comics/2016-07-01.gif" border="0" alt="July 1, 2016: Sweep 5" />', channel: c)
